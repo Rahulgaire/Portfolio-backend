d30cab2a8f0e12f20d038ec3db2eb7e5e13802b7
Rahulgaire <gairerahul334@gmail.com>
2025-05-14 14:21:53 +0530

    UI Link

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const app = express()
 app.use(express.json())
 app.use(cors({
     // origin: 'http://localhost:5173', 
-    origin: 'https://portfolio-frontend-vdfv.onrender.com', 
+    origin: 'https://portfolio-frontend-82aq.onrender.com', 
+
 }
 ))
 app.get('/',(req,res)=>{

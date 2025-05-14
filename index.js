d62f0e5332b79d3901cb939b39efd99f67f06dcd
Rahulgaire <gairerahul334@gmail.com>
2025-05-14 14:01:30 +0530

    Deployment Test

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const port = process.env.PORT || 5000
 const app = express()
 app.use(express.json())
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    // origin: 'http://localhost:5173', 
+    origin: 'https://portfolio-frontend-vdfv.onrender.com', 
 }
 ))
 app.get('/',(req,res)=>{
